536b3ca5fcbe6a6ffb4d4838b703856f087f9ae5
abnerhenriquealves <abner.henrique.biologo@gmail.com>
2026-09-17 15:21:30 +0000

    feat: icones locais pwa adicionados

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "dimdim-v3";
+const ASSETS = [
+  "/ecossistema-casita/",
+  "/ecossistema-casita/index.html",
+  "/ecossistema-casita/manifest.json",
+  "https://cdn.tailwindcss.com"
+];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    }).then(() => clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
